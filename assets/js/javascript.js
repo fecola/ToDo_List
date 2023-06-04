8bacffded1c559a8293f8a1e76ea0ca9f8fe3702
Felipe Colacioppo <felipe.colacioppo@outlook.com>
2023-06-04 17:59:19 -0300

    Primeiro commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+let buttonAdd = document.getElementById('btn-add');
+let nomeTarefa = document.getElementById('inputTask').value;
+let ul = document.getElementById('ulLista');
+
+
+function insertTask() {
+
+    alert(nomeTarefa)
+
+}
